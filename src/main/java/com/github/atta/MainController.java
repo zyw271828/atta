c62d5a06a47a4a1e7f827c1f9c303ade0648c166
zyw271828 <zyw_0618@outlook.com>
2020-12-10 08:57:14 +0800

    Move the calculation to a task

--- src/main/java/com/github/atta/MainController.java
+++ src/main/java/com/github/atta/MainController.java
@@ -3,7 +3,10 @@ package com.github.atta;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
 
@@ -23,19 +26,49 @@ public class MainController {
 
     @FXML
     void onErasureBtnClick(ActionEvent event) {
-        // TODO: Show progress bar
-
         String inputText = inputArea.getText();
-        String translatedText = Translator.translate(inputText);
-        String invTranslatedText = InvTranslator.invTranslate(translatedText);
-        String fixInfo = Checker.check(inputText, invTranslatedText);
-        String fixedText = Fixer.fix(invTranslatedText, fixInfo);
-        String validationResult = Verifier.verify(inputText, fixedText);
 
-        outputArea.setText(fixedText);
+        if (inputText.equals("")) {
+            inputArea.setPromptText("Please enter some text here.");
+            return;
+        } else {
+            erasureBtn.setDisable(true);
+            exitBtn.setDisable(true);
 
-        // TODO: Display validationResult in a TextArea
-        System.out.println(validationResult);
+            Task<Boolean> task = new Task<Boolean>() {
+                @Override
+                protected Boolean call() throws Exception {
+                    // TODO: Show progress bar
+
+                    String translatedText = Translator.translate(inputText);
+                    String invTranslatedText = InvTranslator.invTranslate(translatedText);
+                    String fixInfo = Checker.check(inputText, invTranslatedText);
+                    String fixedText = Fixer.fix(invTranslatedText, fixInfo);
+                    String validationResult = Verifier.verify(inputText, fixedText);
+
+                    outputArea.setText(fixedText);
+
+                    // TODO: Display validationResult in a TextArea
+                    System.out.println(validationResult);
+
+                    erasureBtn.setDisable(false);
+                    exitBtn.setDisable(false);
+
+                    return true;
+                }
+            };
+
+            task.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+                @Override
+                public void handle(WorkerStateEvent wse) {
+                    if (task.getValue()) {
+
+                    }
+                }
+            });
+
+            new Thread(task).start();
+        }
     }
 
     @FXML
