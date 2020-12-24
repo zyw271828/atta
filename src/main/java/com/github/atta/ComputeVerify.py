a6019fdc846c732477c8b494016c2206a17b8b8b
zyw271828 <zyw_0618@outlook.com>
2020-12-24 18:46:20 +0800

    Clean up directory & format code

--- src/main/java/com/github/atta/ComputeVerify.py
+++ src/main/java/com/github/atta/ComputeVerify.py
@@ -2,14 +2,14 @@ from pycocoevalcap.bleu.bleu import Bleu
 from pycocoevalcap.cider.cider import Cider
 from pycocoevalcap.meteor.meteor import Meteor
 
-if __name__=='__main__':
-    with open("in.txt","r") as f:
-        data=f.readlines()
-        gts={1:[data[0]]}
-        res={1:[data[1]]}
-        #print(gts)
-        #print(res)
-    scorer=Bleu()
-    scorer._n=4
-    ret,rets=scorer.compute_score(gts,res)
-    print(100*(1-ret[3]))
+if __name__ == '__main__':
+    with open("in.txt", "r") as f:
+        data = f.readlines()
+        gts = {1: [data[0]]}
+        res = {1: [data[1]]}
+        # print(gts)
+        # print(res)
+    scorer = Bleu()
+    scorer._n = 4
+    ret, rets = scorer.compute_score(gts, res)
+    print(100 * (1 - ret[3]))
