a1b1d1e241ca635ac05954d5ff46cc603758f812
zyw271828 <zyw_0618@outlook.com>
2020-12-24 11:23:00 +0800

    Remove Fixer

--- src/main/java/com/github/atta/Fixer.java
+++
@@ -1,7 +0,0 @@
-package com.github.atta;
-
-public class Fixer {
-	public static String fix(String invTranslatedText, String fixInfo) {
-		return "";
-	}
-}
--- src/main/java/com/github/atta/Verifier.java
+++ src/main/java/com/github/atta/Verifier.java
@@ -1,7 +1,7 @@
 package com.github.atta;
 
 public class Verifier {
-    public static String verify(String inputText, String fixedText) {
+    public static String verify(String inputText, String invTranslatedText) {
         return "";
     }
 }
