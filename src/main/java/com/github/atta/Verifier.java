2fd155e5648869f54e7856229c59cb2ba16501b5
gmhshanest <917721928@qq.com>
2020-12-24 17:41:11 +0800

    primary verifier

--- src/main/java/com/github/atta/Verifier.java
+++ src/main/java/com/github/atta/Verifier.java
@@ -1,7 +1,41 @@
 package com.github.atta;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.sun.java.util.jar.pack.Package.File;
+
+
 public class Verifier {
     public static String verify(String inputText, String invTranslatedText) {
-        return "";
+    	Process process;
+    	
+    	File f=new File("in.txt");
+		FileOutputStream fos1=new FileOutputStream(f);
+		OutputStreamWriter dos1=new OutputStreamWriter(fos1);
+		dos1.write(inputText);
+		dos1.write(invTranslatedText);
+		dos1.close();
+    	
+    	try {
+    		process=Runtime.getRuntime().exec("python ComputeVerify.py");
+    		BufferedReader in=new BufferedReader(new InputStreamReader(process.getInputStream()));
+    		String line=null;
+    		String tmp=null;
+    		while((line=in.readLine())!=null) {
+    			tmp=line;
+    		}
+    		in.close();
+    		process.waitFor();
+    	}catch (IOException e) {
+			// TODO: handle exception
+    		e.printStackTrace();
+		}catch (InterruptedException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+        return tmp;
     }
 }
