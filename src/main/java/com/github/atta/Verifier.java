f4163628da19a08e6465f6bb3e1971825af19275
zyw271828 <zyw_0618@outlook.com>
2020-12-24 21:21:24 +0800

    Update Verifier

--- src/main/java/com/github/atta/Verifier.java
+++ src/main/java/com/github/atta/Verifier.java
@@ -1,40 +1,27 @@
 package com.github.atta;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Verifier {
 	public static String verify(String inputText, String invTranslatedText) {
-		Process process;
-
-		File f = new File("in.txt");
-		FileOutputStream fos1 = new FileOutputStream(f);
-		OutputStreamWriter dos1 = new OutputStreamWriter(fos1);
-		dos1.write(inputText);
-		dos1.write(invTranslatedText);
-		dos1.close();
+		String s = null;
 
 		try {
-			process = Runtime.getRuntime().exec("python ComputeVerify.py");
-			BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			String line = null;
-			String tmp = null;
-			while ((line = in.readLine()) != null) {
-				tmp = line;
+			Process p = Runtime.getRuntime().exec(new String[] { "python3",
+					"./src/main/java/com/github/atta/verify.py", inputText, invTranslatedText });
+
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			StringBuilder stringBuilder = new StringBuilder();
+			while ((s = stdInput.readLine()) != null) {
+				stringBuilder.append(s);
 			}
-			in.close();
-			process.waitFor();
+			s = stringBuilder.toString();
 		} catch (IOException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO: handle exception
 			e.printStackTrace();
 		}
-		return tmp;
+
+		return s;
 	}
 }
