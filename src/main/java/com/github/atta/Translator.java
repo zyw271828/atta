bbbba9e962db8890ba3ec07f7d5f76f0ff69c626
zyw271828 <zyw_0618@outlook.com>
2020-12-25 10:31:29 +0800

    Add exception handling

--- src/main/java/com/github/atta/Translator.java
+++ src/main/java/com/github/atta/Translator.java
@@ -1,6 +1,5 @@
 package com.github.atta;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
@@ -13,10 +12,11 @@ import org.json.JSONObject;
 public class Translator {
     private static final String RAPIDAPI_KEY = "RAPIDAPI_KEY";
 
-    public static String translate(String inputText, String source, String target)
-            throws IOException, InterruptedException {
+    public static String translate(String inputText, String source, String target) {
         String payload = "q=" + URLEncoder.encode(inputText, StandardCharsets.UTF_8) + "&source=" + source + "&target="
                 + target;
+        JSONObject jsonObj = null;
+        String translatedText = "";
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://google-translate1.p.rapidapi.com/language/translate/v2"))
@@ -25,10 +25,15 @@ public class Translator {
                 .header("x-rapidapi-host", "google-translate1.p.rapidapi.com")
                 .method("POST", HttpRequest.BodyPublishers.ofString(payload)).build();
 
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        JSONObject jsonObj = new JSONObject(response.body());
-        String translatedText = jsonObj.getJSONObject("data").getJSONArray("translations").getJSONObject(0)
-                .getString("translatedText");
+        try {
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request,
+                    HttpResponse.BodyHandlers.ofString());
+            jsonObj = new JSONObject(response.body());
+            translatedText = jsonObj.getJSONObject("data").getJSONArray("translations").getJSONObject(0)
+                    .getString("translatedText");
+        } catch (Exception e) {
+            translatedText = jsonObj.toString();
+        }
 
         return translatedText;
     }
