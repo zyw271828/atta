af09b5f84b2807949620f5c6828df950b9ff8fd7
zyw271828 <zyw_0618@outlook.com>
2020-12-26 22:49:20 +0800

    Update file separator for Windows

--- src/main/java/com/github/atta/Verifier.java
+++ src/main/java/com/github/atta/Verifier.java
@@ -1,27 +1,28 @@
 package com.github.atta;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Verifier {
-	public static String verify(String inputText, String invTranslatedText) {
-		String s = null;
+    public static String verify(String inputText, String invTranslatedText) {
+        String s = null;
+        final String path = "./src/main/java/com/github/atta/verify.py".replaceAll("/", File.separator);
 
-		try {
-			Process p = Runtime.getRuntime().exec(new String[] { "python3",
-					"./src/main/java/com/github/atta/verify.py", inputText, invTranslatedText });
+        try {
+            Process p = Runtime.getRuntime().exec(new String[] { "python3", path, inputText, invTranslatedText });
 
-			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			StringBuilder stringBuilder = new StringBuilder();
-			while ((s = stdInput.readLine()) != null) {
-				stringBuilder.append(s);
-			}
-			s = stringBuilder.toString();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((s = stdInput.readLine()) != null) {
+                stringBuilder.append(s);
+            }
+            s = stringBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		return s;
-	}
+        return s;
+    }
 }
