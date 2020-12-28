3d5d09603a2a864500997ef873eb5ad5c6356003
zyw271828 <zyw_0618@outlook.com>
2020-12-28 21:31:44 +0800

    Remove mvn install command

--- config_spice.sh
+++ config_spice.sh
@@ -12,9 +12,6 @@ unzip SPICE-1.0.zip -d lib/
 
 echo "Configuring..."
 ./lib/SPICE-1.0/get_stanford_models.sh
-mvn install:install-file -Dfile=./lib/SPICE-1.0/spice-1.0.jar \
-    -DgroupId=edu.anu.spice -DartifactId=spice -Dversion=1.0 -Dpackaging=jar \
-    -DgeneratePom=true
 
 echo "Cleaning up"
 rm ./SPICE-1.0.zip
